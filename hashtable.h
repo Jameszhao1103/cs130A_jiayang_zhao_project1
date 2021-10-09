eb321932c5fb581c3bd026ffd7d5f84c324f151c
JamesZ <jiayang_zhao@ucsb.edu>
2021-10-09 11:14:35 -0700

    Add files via upload

---
+++ hashtable.h
@@ -0,0 +1,92 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <iostream>
+#include <cstdlib> 
+#include <vector>
+
+int convert1(std::string s, int p, int c, int index){
+    if (index == s.length()-1){
+        return int(s[index])%p;
+    }else{
+        int temp = int(s[index]);
+        int new_index = index + 1;
+        return (temp + c * (convert1(s,p,c,new_index)))%p;
+    }
+}
+
+class Hash{
+    public:
+    struct item{
+        int num_item;
+        std::vector<std::string> element;
+
+        
+
+        void initial(int num_object){
+            element.resize(num_object);
+            for (int i = 0; i < element.size();i++){
+                element.at(i) = "";
+            }
+        }
+    };
+
+    std::vector<item> hashmap;
+
+    void createmap(int size){
+        hashmap.resize(size);
+    }
+
+    void resizelement(int index, int size){
+        hashmap[index].element.resize(size);
+        hashmap[index].num_item = size;
+        for (int i = 0; i < hashmap[index].element.size();i++){
+            hashmap[index].element.at(i) = "";
+        }
+    }
+
+    void insertword(int bucket_num, std::string word){
+        for (int i = 0; i < hashmap[bucket_num].element.size();i++){
+            if(hashmap[bucket_num].element.at(i).length()==0){
+                hashmap[bucket_num].element.at(i) = word;
+                break;
+            }
+        }
+    }
+
+    int sizeofbucket(int bucket_num){
+        return hashmap[bucket_num].num_item;
+    }
+    
+    std::vector<std::string> findword(int bucket_num){
+        return hashmap[bucket_num].element;
+    }
+
+    std::vector<int> queryword(std::string query_word, int p, int c){
+        //get hash number of this string
+        std::vector<int> not_found = {-999, -999};
+        int stringidx = convert1(query_word, p, c, 0);
+        if(hashmap[stringidx].num_item==0){
+            return not_found;
+        }else{
+            for (int i = 0; i < hashmap[stringidx].num_item;i++){
+                if(hashmap[stringidx].element[i]==query_word){
+                    return std::vector<int>{stringidx, i};
+                }
+            }
+            return not_found;
+        }
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
